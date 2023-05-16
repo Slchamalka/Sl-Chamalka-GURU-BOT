c95e3ad66274b1dca626086f1e57f119817812d1
Slchamalka <133760502+Slchamalka@users.noreply.github.com>
2023-05-17 05:10:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94758010495', 'Owner ', true],
+  ['94758010495', 'Owner', true],
   ['94758010495', 'Sl Chamalka', true], 
   [''] 
 ] //Number of owners
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'Sl Chamalka'
+global.botname = 'Chamalka'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
